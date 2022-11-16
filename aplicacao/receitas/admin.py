fcaa46b602f4d723dbbdf7b8914ca4f4975fdcfa
Priscila Barros <priscila.saldanha.barros@gmail.com>
2022-11-16 04:51:39 -0400

    Editar informações mostradas na tela pelo admin

--- aplicacao/receitas/admin.py
+++ aplicacao/receitas/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas( admin.ModelAdmin):
+    list_display = ('id','nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+admin.site.register(Receita,ListandoReceitas)
 
